6bcf0acdb5694118be9b061d411ebeaf72b71ac6
jessy2022 <97400317+jessy2022@users.noreply.github.com>
2022-01-10 17:19:32 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+   
+  <link rel="stylesheet" media="screen" href="login.css" >
+</head>
+<body>
+  <table align="center" border="0" bgcolor="#B22222" width="540" cellpadding="9" cellspacing="0" height="525">
+          <tr>
+            <td colspan="3" height="2"><img src="banner.jpg" width="860"></td>
+          </tr>
+          <tr>
+            <td colspan="3" bgcolor="#FF0000" height="1" align="center">
+        <font size="4">
+           
+            <a href="login2.php">LOGIN</a> 
+            
+            
+          </font>
+            </td>
+          </tr>
+          <tr>
+            <td width="50%" align="center" bgcolor="#B22222">
+       
+<div class="ex">
+       <h2 class="bg-primary"><font color="#fff">Online Inmate management System</font></h2>
+
+<tr>
+  <?php
+           include("footer.php");
+                ?>
+</tr>
+
+</table>
+</body>
+</html>
